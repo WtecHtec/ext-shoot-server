2765bd41672e98e06bf6228f7976b8bff055bfdf
river <laolei@forkway.cn>
2024-04-05 17:25:04 +0800

    chore: add test

--- test/pages.test.js
+++ test/pages.test.js
@@ -9,9 +9,9 @@
 // }
 
 // main()
-// describe.skip('End-to-End Test', () => {
-// 	test("mock test", () => {
-// 		expect(1).toBe(1)
-// 	})
+describe.skip('End-to-End Test', () => {
+	test("mock test", () => {
+		expect(1).toBe(1)
+	})
 	
 // });
